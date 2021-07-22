cdfc69680fa1d9999ee95c47dd156d4ec62c374d
oheunhye <41795203+oheunhye@users.noreply.github.com>
2021-07-22 17:00:38 +0900

    회원가입 기능 추가

---
+++ AspnetCoreStudy/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using AspnetCoreStudy.DataContext;
+using AspnetCoreStudy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCoreStudy.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// 로그인
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 회원가입
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            //유효성 검사: Model의 Required 값 확인
+            if (ModelState.IsValid)
+            {
+                using(var db = new AspnetCoreStudyDbContext())
+                {
+                    db.Users.Add(model);    //메모리상의 추가
+                    db.SaveChanges();        //Commit
+                }
+
+                return RedirectToAction("Index","Home");
+            }
+
+            return View(model);
+        }
+
+    }
+}
--- AspnetCoreStudy/Models/User.cs
+++ AspnetCoreStudy/Models/User.cs
@@ -14,19 +14,19 @@ namespace AspnetCoreStudy.Models
         /// <summary>
         /// 사용자 이름
         /// </summary>
-        [Required]  // Not Null 설정
+        [Required(ErrorMessage ="사용자 이름을 입력하세요.")]  // Not Null 설정
         public string Name { get; set; }
 
         /// <summary>
         /// 사용자 ID
         /// </summary>
-        [Required]  // Not Null 설정
+        [Required(ErrorMessage ="사용자 ID를 입력하세요.")]  // Not Null 설정
         public string Id { get; set; }
 
         /// <summary>
         /// 사용자 비밀번호
         /// </summary>
-        [Required]  // Not Null 설정
+        [Required(ErrorMessage ="비밀번호를 입력하세요.")]  // Not Null 설정
         public string Password { get; set; }
 
     }
